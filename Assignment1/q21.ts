4ef03fedfb63846f3685147fada3e26641913ab7
M Ehtesham Ul Hassan Malik <malikehtesham.ths@gmail.com>
2024-02-25 21:49:53 +0500

    add q21.ts

---
+++ Assignment1/q21.ts
@@ -0,0 +1,36 @@
+//typeScript objects representing students
+let student1 : {name: string, fname: string, education: string}={
+    name: "Ehtesham",
+    fname: "Malik Azmat",
+    education: "BSSE (Continue)"
+}
+let student2 : {name: string, fname: string, education: string}={
+    name: "Ahmed",
+    fname: "Abdul Razzaq",
+    education: "Matric"
+}
+let student3 : {name: string, fname: string, education: string}={
+    name: "Saba",
+    fname: "Malik Idrees",
+    education: "M Phil"
+}
+
+
+//printing information of student objects
+console.log("Student 1")
+console.log("Name: ",student1.name)
+console.log("Father Name: ",student1.fname)
+console.log("Education: ",student1.education)
+console.log("\n")
+console.log("Student 2")
+console.log("Name: ",student2.name)
+console.log("Father Name: ",student2.fname)
+console.log("Education: ",student2.education)
+console.log("\n")
+console.log("Student 3")
+console.log("Name: ",student3.name)
+console.log("Father Name: ",student3.fname)
+console.log("Education: ",student3.education)
+console.log("\n")
+
+
