5a543c56de63cf6f7a324979aa082e73d61b9e87
M Ehtesham Ul Hassan Malik <153924478+M-Ehtesham-Ul-Hassan-Malik@users.noreply.github.com>
2024-03-17 17:29:21 +0500

    Delete Assignment1/q14.js

--- Assignment1/q14.js
+++
@@ -1,4 +0,0 @@
-var guest_list = ["Ubaid Shah Shb", "Asif", "Khalil Chishti"];
-for (var i = 0; i < guest_list.length; i++) {
-    console.log("Mr", guest_list[i], "lets have a dinner together.");
-}
