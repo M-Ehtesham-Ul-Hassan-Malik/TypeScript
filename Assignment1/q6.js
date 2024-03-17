90515f1777571bff3a6f8a4ea626a81808aced05
M Ehtesham Ul Hassan Malik <153924478+M-Ehtesham-Ul-Hassan-Malik@users.noreply.github.com>
2024-03-17 17:07:19 +0500

    Delete Assignment1/q6.js

--- Assignment1/q6.js
+++
@@ -1,5 +0,0 @@
-var person_name = "Amjad";
-//printing name with some white space charectors
-console.log("\t", person_name, "\n");
-//printing name without any white space charectors
-console.log(person_name);
