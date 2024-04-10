9830b0f7bc269a99bc01e67eb25011d737d6ae2d
M Ehtesham Ul Hassan Malik <malikehtesham.ths@gmail.com>
2024-04-10 21:12:07 +0500

    add cli atm machine

---
+++ CLI-ATM-Machine/README.md
@@ -0,0 +1,50 @@
+### Project Name: ATM Machine Application
+
+#### Description
+A simple ATM console application built with Node.js and inquirer.js that simulates basic banking operations such as depositing money, withdrawing money, checking balance, and donating to NGOs.
+
+#### Features
+- Login with a secret PIN.
+- Deposit money into the account.
+- Withdraw money from the account (with validation for insufficient funds).
+- Check the current account balance.
+- Donate money to a selected NGO.
+
+#### Installation
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/M-Ehtesham-Ul-Hassan-Malik/TypeScript.git
+   ```
+
+2. Navigate to the project directory:
+   ```bash
+   cd CLI-ATM-MACHINE
+   ```
+
+3. Install dependencies:
+   ```bash
+   npm install inquirer
+   ```
+
+#### Usage
+1. Start the application:
+   ```bash
+   npm start
+   ```
+
+2. Follow the prompts to perform desired operations:
+   - Enter your secret PIN to login.
+   - Choose an operation from the provided list (Deposit Amount, Withdraw Amount, Check Balance, Donate to NGO).
+   - Provide necessary inputs as prompted (e.g., amount to deposit/withdraw, NGO for donation).
+
+## Versions
+Version 1.0.0: Initial release with basic banking operations such as depositing money, withdrawing money, checking balance, and donating to NGOs.
+
+#### Dependencies
+- [inquirer](https://www.npmjs.com/package/inquirer): Used for interactive command-line prompts.
+
+#### License
+This project is licensed under the [ISC License](LICENSE).
+
+#### Author
+M Ehtesham Ul Hassan Malik
---
+++ CLI-ATM-Machine/index.ts
@@ -0,0 +1,112 @@
+#! usr/bin/env Node
+
+import inquirer from "inquirer"
+let myPin = 4824;
+let myBalance = 50000; //Dollars
+
+
+const pinAns = await inquirer.prompt({
+  name: "pin",
+  type: "number",
+  message: "Please enter your secret PIN to proceed:",
+});
+
+
+if (pinAns.pin === myPin) {
+  console.log("Login was successful!");
+
+  const operations = await inquirer.prompt([
+    {
+      name: "opr",
+      type: "list",
+      message: "Please select the operation you want to perform:",
+      choices: [
+        "Deposit Amount",
+        "Withdraw Amount",
+        "Check Balance",
+        "Donate to NGO",
+      ],
+    },
+  ]);
+
+
+  if (operations.opr === "Deposit Amount") {
+    const amountAns = await inquirer.prompt([
+      {
+        name: "amount",
+        type: "number",
+        message: "Please enter amount you want to deposit:",
+      },
+    ]);
+
+    myBalance += amountAns.amount;
+    console.log("your current balance is " + myBalance);
+  }
+
+
+
+  else if (operations.opr === "Withdraw Amount") {
+    const amountAns = await inquirer.prompt([
+        {
+          name: "amount",
+          type: "number",
+          message: "Please enter amount you want to withdraw:",
+        },
+      ]);
+
+      console.log("Withdrawl Amount: " +amountAns.amount);
+      console.log("Current Balance: " +myBalance);
+
+      if (amountAns.amount > myBalance) {
+        console.log("Insufficient funds. Withdrawal failed.");
+      } else {
+        myBalance -= amountAns.amount;
+        console.log("Your current balance is " + myBalance);
+      }
+}
+
+
+else if (operations.opr === "Check Balance") {
+      console.log("your current balance is " + myBalance);
+    }
+
+
+else if(operations.opr === "Donate to NGO"){
+
+    const ngoDetails = await inquirer.prompt(
+        [
+            {
+                name:"ngo",
+                type:"list",
+                message:"Which NGO do you want to donate: ",
+                choices:["Dawat-e-Islami","Sylani Welfare Trust","JDC","Muslim Hands", "Al-Mustafa Welfare Trust", "Al-Khidmat Foundation", "Baitusalam"]
+            },
+            {
+                name:"donation",
+                type:"number",
+                message:"How much money do you want to donate:"
+            }
+        ]
+    )
+
+    console.log("Donation Amount: " +ngoDetails.donation);
+    console.log("Current Balance: " +myBalance);
+
+      if (ngoDetails.donation > myBalance) {
+        console.log("Insufficient funds. Transfer failed.");
+      } else {
+        myBalance -= ngoDetails.donation;
+        console.log("You have transfered successfully " + ngoDetails.donation + " rupees to "+ngoDetails.ngo);
+        console.log("Your current balance is " + myBalance);
+      }
+
+
+}
+}
+
+
+
+
+else{
+    console.log("Incorrect PIN");
+}
