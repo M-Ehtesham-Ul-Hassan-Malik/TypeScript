4505870773c2a2b65f1a8f9b44933f86acf2fdf9
M Ehtesham Ul Hassan Malik <malikehtesham.ths@gmail.com>
2024-04-26 15:31:40 +0500

    bug fix q40.ts

--- Assignment1/q40.ts
+++ Assignment1/q40.ts
@@ -1,13 +1,14 @@
-function make_album(artist: string, title: string, track?: number){
-    let album = {artist , title}
-     if (track !== undefined) {
-         album.track = track; // Assign track to album if provided
-     }
+function make_album(artist: string, title: string, track?: number) {
+    let album = { artist, title } as { artist: string; title: string; track?: number };
+
+    if (track !== undefined) {
+        album.track = track; // Assign track to album if provided
+    }
 
     return album;
 }
+
+// Function calls
 console.log(make_album("First Artist", "The First Album"));
 console.log(make_album("Second Artist", "The Second Album"));
 console.log(make_album("Last Artist", "The Last Album", 33));
-
-
